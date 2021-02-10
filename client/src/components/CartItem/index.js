423e16261cd595230628f95dc18dd37ef503803b
fintank <finapptank@gmail.com>
2021-02-10 09:55:27 -0500

    updated indexeddb

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
@@ -8,26 +9,31 @@ const CartItem = ({ item }) => {
 
     const removeFromCart = item => {
         dispatch({
-            type: REMOVE_FROM_CART,
-            _id: item._id
+          type: REMOVE_FROM_CART,
+          _id: item._id
         });
-    };
+        idbPromise('cart', 'delete', { ...item });
+      };
 
     const onChange = (e) => {
         const value = e.target.value;
 
         if (value === '0') {
             dispatch({
-                type: REMOVE_FROM_CART,
-                _id: item._id
+              type: REMOVE_FROM_CART,
+              _id: item._id
             });
-        } else {
+          
+            idbPromise('cart', 'delete', { ...item });
+          } else {
             dispatch({
-                type: UPDATE_CART_QUANTITY,
-                _id: item._id,
-                purchaseQuantity: parseInt(value)
+              type: UPDATE_CART_QUANTITY,
+              _id: item._id,
+              purchaseQuantity: parseInt(value)
             });
-        }
+          
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+          }
     };
     return (
         <div className="flex-row">
